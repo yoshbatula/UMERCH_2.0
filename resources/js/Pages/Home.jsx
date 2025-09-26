2c681a216e7b4472f28ca57b7c39f4ffc39f6acd
yoshbatula <yoshbatula2@gmail.com>
2025-09-26 12:25:24 +0800

    progress of learning react laravel

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1 className="text-5xl font-bold">Hello</h1>
+    );
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @viteReactRefresh
+    <title>My App</title>
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
