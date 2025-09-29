763baef2d1e42ba6b42f40d3ec3f869019b543e1
yoshbatula <yoshbatula2@gmail.com>
2025-09-29 13:16:08 +0800

    umerch logo in the https

--- resources/js/Layouts/navbar.jsx
+++ resources/js/Layouts/navbar.jsx
@@ -30,6 +30,11 @@ export default function navbar({children}) {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                 </div>
+
+                {/* Sign-In */}
+                <div>
+                    <Link href="/login" className="mx-2 text-white text-[14px]">SIGN IN</Link>
+                </div>
             </header>
             <main>
                 {children}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     @viteReactRefresh
-    <title>My App</title>
+    <title>UMERCH</title>
+    <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/Umerch-Logo.png') }}"/>
     @vite('resources/js/app.jsx')
     @inertiaHead
   </head>
