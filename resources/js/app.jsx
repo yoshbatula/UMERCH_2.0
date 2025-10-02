68723d4200483c1a70d92cf39d52b2ee63cb8b24
yoshbatula <yoshbatula2@gmail.com>
2025-10-02 14:28:39 +0800

    progress for now

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,10 +7,9 @@ import Layout from './Layouts/navbar';
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    let page = pages[`./Pages/${name}.jsx`];
-    page.default.layout = 
-    page.default.layout || ((page) => <Layout children={page}/>);
-    return page;
+    let page = pages[`./Pages/${name}.jsx`]
+    page.default.layout = page.default.layout || (page => <Layout children={page} />)
+    return page
   },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
