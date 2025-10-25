4233ec9e20ea254ad75a21430e29062744026acd
yoshbatula <yoshbatula2@gmail.com>
2025-10-25 19:17:38 +0800

    finalizing some features

--- resources/js/Pages/User-page/Authentication.jsx
+++ resources/js/Pages/User-page/Authentication.jsx
@@ -116,6 +116,7 @@ export default function Authentication({ user, errors: pageErrors }) {
         }
     }, [resendCooldown]);
 
+    // adding something
     return (
         <>
             <div className="bg-[#F6F6F6] flex flex-col justify-center items-center w-full h-screen">
