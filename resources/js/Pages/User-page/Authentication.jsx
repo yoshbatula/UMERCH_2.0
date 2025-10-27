4a27f657e993526a1a3d08da6a3f9983b4dfce2a
yoshbatula <yoshbatula2@gmail.com>
2025-10-27 22:44:22 +0800

    new features

--- resources/js/Pages/User-page/Authentication.jsx
+++ resources/js/Pages/User-page/Authentication.jsx
@@ -119,6 +119,7 @@ export default function Authentication({ user, errors: pageErrors }) {
     // adding something
     // finalizing
     // just adding something new features
+    // why is it not working?
     return (
         <>
             <div className="bg-[#F6F6F6] flex flex-col justify-center items-center w-full h-screen">
