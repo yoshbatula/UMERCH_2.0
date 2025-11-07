5d42f9c675c5f64b5ab5191e2daa4d706aaaf652
yoshbatula <yoshbatula2@gmail.com>
2025-11-07 23:27:26 +0800

    just adding some new features for the inventory

--- resources/js/Pages/Admin-page/Inventory/AddProduct.jsx
+++ resources/js/Pages/Admin-page/Inventory/AddProduct.jsx
@@ -5,7 +5,7 @@ import Danger from '../../../../images/Danger.svg'
 export default function AddProduct() {
     return (
         <div className='py-9 px-19 flex flex-col'>
-            <div className='text-[30px] mb-4'>
+            <div className='text-[32px] mb-4'>
                 <h1 className='font-bold'>Inventory</h1>
                 <div className='mt-1'>
                     <h1 className='text-[20px] mb-4'>Welcome to Inventory</h1>
@@ -38,8 +38,38 @@ export default function AddProduct() {
                     </div>
                 </div>
             </div>
+            {/* Add Product */}
+            <div className='mt-8 relative'>
+                <div className='font-semibold text-[30px]'>
+                    <h1>Add Product</h1>
+                </div>
+                <div className='mt-3 flex flex-row justify-between'>
+                    <div className='relative'>
+                        <svg 
+                            className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5"
+                            fill="none"
+                            stroke="#9C0306"
+                            viewBox="0 0 24 24"
+                        >
+                            <path 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round" 
+                                strokeWidth={2} 
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" 
+                            />
+                        </svg>
+                        <input 
+                            type="text"
+                            placeholder='Search Products'
+                            className="block w-80 pl-10 pr-24 py-2 border border-gray-300 rounded-lg leading-5 bg-[#F9FAFB] placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-[#9C0306] text-sm"
+                        />
+                    </div>
+                    <div className='bg-[#9C0306] text-white text-[16px] flex flex-row font-semibold justify-center items-center w-[166px] h-[44px] rounded-[20px] hover: cursor-pointer'>
+                        <button className='hover:cursor-pointer'>Add Product</button>
+                    </div>
+                </div>
+            </div>
         </div>
-       
     );
 }
 
