b01888bdee82765fdba8f3f625205b0b8e099cfc
yoshbatula <yoshbatula2@gmail.com>
2025-11-22 15:36:29 +0800

    adding new features

--- resources/js/Pages/Admin-page/modals/AddProductModal.jsx
+++ resources/js/Pages/Admin-page/modals/AddProductModal.jsx
@@ -85,7 +85,7 @@ export default function AddProductModal({ isOpen, onClose }) {
                 </div>
             </div>
             </div>
-            
+            {/* new features */}
         </div>
     );
 }
